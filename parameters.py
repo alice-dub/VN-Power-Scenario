06b6b281b767e1adee2344a228003d8317cc3de5
Minh Ha-Duong <minh.haduong@gmail.com>
2017-05-02 16:27:15 +0200

    Read the installed capacity plan, group by technology, replace Nuke by Thermal and addup by year.

--- parameters.py
+++ parameters.py
@@ -6,10 +6,50 @@
 #
 #
 import pandas as pd
+pd.set_option('display.max_rows', 1000)
 
-PDP7A_annex1_file = "data/PDP7A/annex1.txt"
-t = pd.read_fwf(PDP7A_annex1_file)
-print(t)
+PDP7A_annex1 = pd.read_fwf("data/PDP7A/annex1.txt",
+                           usecols=["year", "id", "tech", "capacity_MW"],
+                           )
+
+PDP7A_annex1.replace({"tech": {"TD": "Hydro",
+                               "ND": "Thermal",
+                               "TBKHH": "GasCC",
+                               "NLTT": "Renewable",
+                               "NMDSK": "Renewable",  # Biomass
+                               "DG": "Renewable",  # Wind
+                               "DMT": "Renewable",  # Solar
+                               "TDN": "Renewable",  # Small hydro
+                               "DHN": "Nuclear",    # Abandonned
+                               }},
+                     inplace=True
+                     )
+
+# print(PDP7A_annex1)
+
+print("Total capacity added unded PDP7A, 2016-2030 (MW)")
+print(PDP7A_annex1.groupby("tech").capacity_MW.sum())
+print()
+
+print("Total capacity added unded PDP7A, 2016-2030 (MW), Nuclear replaced by Thermal")
+PDP7A_annex1.replace({"tech": {"Nuclear": "Thermal"}},
+                     inplace=True
+                     )
+print()
+# print(PDP7A_annex1.groupby("tech").capacity_MW.sum())
+# print()
+
+baseline_capacity_added = PDP7A_annex1.groupby(["year", "tech"]).capacity_MW.sum()
+baseline_capacity_added = baseline_capacity_added.unstack().fillna(0)
+print("Annual (MW)")
+print(baseline_capacity_added)
+print()
+
+print("Cumulative since 2016 (MW)")
+baseline_capacity_new = baseline_capacity_added.cumsum()
+print(baseline_capacity_new)
+
+
 
 """Input parameters of the model"""
 
