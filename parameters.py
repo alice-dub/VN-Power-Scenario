d42753c7af38d4b7589eba427873ae4323afa0e2
Minh Ha-Duong <minh.haduong@gmail.com>
2017-05-03 19:22:37 +0200

    Add 2015 capacities based on EVN 2016 annual report. Separates Oil fuel

--- parameters.py
+++ parameters.py
@@ -8,13 +8,18 @@
 import pandas as pd
 pd.set_option('display.max_rows', 1000)
 
+#
+# Read the prepared data
+#
+
 PDP7A_annex1 = pd.read_fwf("data/PDP7A/annex1.txt",
                            usecols=["year", "id", "tech", "capacity_MW"],
                            )
 
 PDP7A_annex1.replace({"tech": {"TD": "Hydro",
-                               "ND": "Thermal",
-                               "TBKHH": "GasCC",
+                               "ND": "Coal",
+                               "NDHD": "Oil",
+                               "TBKHH": "Gas",
                                "NLTT": "Renewable",
                                "NMDSK": "Renewable",  # Biomass
                                "DG": "Renewable",  # Wind
@@ -25,23 +30,38 @@ PDP7A_annex1.replace({"tech": {"TD": "Hydro",
                      inplace=True
                      )
 
+# Deal with ND hoa dao is oil not coal
+
 # print(PDP7A_annex1)
 
-print("Total capacity added unded PDP7A, 2016-2030 (MW)")
+print("Summary of 2016-2030 new capacity listed in PDP7A annex 1, MW")
 print(PDP7A_annex1.groupby("tech").capacity_MW.sum())
 print()
 
-print("Total capacity added unded PDP7A, 2016-2030 (MW), Nuclear replaced by Thermal")
-PDP7A_annex1.replace({"tech": {"Nuclear": "Thermal"}},
-                     inplace=True
-                     )
+# Source : EVN Annual report 2016, pages 11
+capacity_2015 = {"Hydro": 14636, "Coal": 12903, "Oil": 875, "Gas": 7998, "Renewable": 2141}
+# TODO: Check that all units listed in PDP7A Annex 1
+#  are not in the above table
+
+# TODO: update the PDP using the "investment projects" of the EVN 2016 report
+
+#
+# define the baseline scenario
+#
+print("***** Baseline *****")
 print()
+
+# The National Assembly voted to cancel the nuclear program
+baseline = PDP7A_annex1.replace({"tech": {"Nuclear": "Coal"}})
+
+# TODO: update with plants removed from the plan like Bac Lieu
+# TODO: remove the (*) plants presented as backup if renewable don't fly
 # print(PDP7A_annex1.groupby("tech").capacity_MW.sum())
 # print()
 
-baseline_capacity_added = PDP7A_annex1.groupby(["year", "tech"]).capacity_MW.sum()
+baseline_capacity_added = baseline.groupby(["year", "tech"]).capacity_MW.sum()
 baseline_capacity_added = baseline_capacity_added.unstack().fillna(0)
-print("Annual (MW)")
+print("Annual new capacity (MW)")
 print(baseline_capacity_added)
 print()
 
