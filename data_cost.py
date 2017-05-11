3afae0114fd3187f27a519f5ef368fb8118a399c
Minh Ha-Duong <minh.haduong@gmail.com>
2017-05-11 08:55:02 +0200

    Plot box+whiskers of observed construction costs

--- data_cost.py
+++ data_cost.py
@@ -36,9 +36,8 @@ OpenEI_construction_cost = pd.read_csv(OpenEI_construction_cost_filename,
                                                 "TechIndex",
                                                 "Technology",
                                                 "Year",
+                                                "PublicationYear",
                                                 "OnghtCptlCostDolPerKw"])
-
-show(OpenEI_construction_cost)
 
 
 def capital_cost(fuel, techindex):
@@ -46,36 +45,40 @@ def capital_cost(fuel, techindex):
     lm = pd.ols(x=data.Year - start_year, y=data.OnghtCptlCostDolPerKw)
     show(fuel, techindex, lm)
     cost_start_year = lm.beta.intercept
-    cost_start_year_SE = lm.std_err.intercept
     trend = lm.beta.x if lm.p_value.x < 0.05 else 0
     s = pd.Series(np.linspace(cost_start_year, cost_start_year + trend * n_year, n_year),
                   index=years,
                   name=fuel).round().astype("int64")
+
     if VERBOSE:
+        data_past = data[data.Year < data.PublicationYear]
+        year_min = data_past.Year.min()
+        year_max = data_past.Year.max()
+        cost_median = data_past.OnghtCptlCostDolPerKw.median()
+        cost_quartile1 = data_past.OnghtCptlCostDolPerKw.quantile(0.25)
+        cost_quartile3 = data_past.OnghtCptlCostDolPerKw.quantile(0.75)
         data.plot.scatter("Year",
                           "OnghtCptlCostDolPerKw",
                           title=fuel + " generation capacity overnight construction cost\n$/kW")
-        plt.plot([start_year, end_year],
-                 [cost_start_year, cost_start_year + trend * (n_year - 1)],
-                 'r-',
-                 linewidth=2.0)
-        # The regression confidence interval does not show the uncertainty in data
-        plt.plot([start_year, end_year],
-                 [cost_start_year + 2 * cost_start_year_SE,
-                  cost_start_year + trend * (n_year - 1) + 2 * cost_start_year_SE],
-                 'r--',
-                 linewidth=1.0)
-        plt.plot([start_year, end_year],
-                 [cost_start_year - 2 * cost_start_year_SE,
-                  cost_start_year + trend * (n_year - 1) - 2 * cost_start_year_SE],
-                 'r--',
-                 linewidth=1.0)
+        # Red line showing the construction costs used in model
+        # Regression is both on observation and projections
+        plt.plot(s.index, s.values, "r-", linewidth=2.0)
+        # Magenta box and whiskers showing quartiles of observed costs
+        plt.plot([year_min, year_max], [cost_median, cost_median], 'm', linewidth=3)
+        plt.plot([year_min, year_max], [cost_quartile1, cost_quartile1], 'm-', linewidth=2)
+        plt.plot([year_min, year_max], [cost_quartile3, cost_quartile3], 'm-', linewidth=2)
+        plt.plot([year_min, year_min], [cost_quartile1, cost_quartile3], 'm-', linewidth=2)
+        plt.plot([year_max, year_max], [cost_quartile1, cost_quartile3], 'm-', linewidth=2)
+        x = (year_min + year_max) / 2
+        plt.plot([x, x], [cost_quartile3, data_past.OnghtCptlCostDolPerKw.max()], 'm-', linewidth=1)
+        plt.plot([x, x], [data_past.OnghtCptlCostDolPerKw.min(), cost_quartile1], 'm-', linewidth=1)
         return int(cost_start_year), trend, s
 
 
 def set_capital_cost(fuel, techindex):
-    a, b, s = capital_cost(fuel, techindex)
-    construction_cost_start_year[fuel], construction_cost_trend[fuel] = a, b
+    cost_start_year, trend, s = capital_cost(fuel, techindex)
+    construction_cost_start_year[fuel] = cost_start_year
+    construction_cost_trend[fuel] = trend
     construction_cost[fuel] = s
 
 construction_cost_start_year = pd.Series(dtype="int64")
